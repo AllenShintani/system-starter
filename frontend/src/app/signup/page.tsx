6c932942b2598ff4fb511eedff6ccfda69c0ac97
AllenShintani <shintaniallen@gmail.com>
2024-10-05 12:23:29 +0900

    fix: use clientをidページに追加

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { useState } from 'react'
 import type { FormEvent } from 'react'
 import 'firebase/compat/auth'
@@ -24,8 +23,6 @@ import {
   Link,
 } from '@mui/material'
 import { trpc } from '@/utils/trpc'
-
-const theme = createTheme()
 
 const translateZodError = (error: ZodIssue) => {
   switch (error.code) {
@@ -88,143 +85,141 @@ export default function SignUp() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container
-        component="main"
-        maxWidth="xs"
+    <Container
+      component="main"
+      maxWidth="xs"
+    >
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
       >
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
+          新規登録
+        </Typography>
+        {error && (
           <Typography
-            component="h1"
-            variant="h5"
+            color="error"
+            variant="body2"
           >
-            新規登録
+            {error}
           </Typography>
-          {error && (
-            <Typography
-              color="error"
-              variant="body2"
-            >
-              {error}
-            </Typography>
-          )}
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+        )}
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: 3 }}
+        >
+          <Grid
+            container
+            spacing={2}
           >
             <Grid
-              container
-              spacing={2}
+              item
+              xs={12}
+              sm={6}
             >
-              <Grid
-                item
-                xs={12}
-                sm={6}
-              >
-                <TextField
-                  autoComplete="given-name"
-                  name="lastName"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="姓"
-                  autoFocus
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={6}
-              >
-                <TextField
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="名"
-                  name="firstName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-              >
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="メールアドレス"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-              >
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="パスワード"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-              >
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      value="allowExtraEmails"
-                      color="primary"
-                    />
-                  }
-                  label="メールマガジンを受け取りますか？"
-                />
-              </Grid>
+              <TextField
+                autoComplete="given-name"
+                name="lastName"
+                required
+                fullWidth
+                id="lastName"
+                label="姓"
+                autoFocus
+              />
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={signupMutation.isLoading}
-            >
-              {signupMutation.isLoading ? '登録中...' : '登録'}
-            </Button>
             <Grid
-              container
-              justifyContent="flex-end"
+              item
+              xs={12}
+              sm={6}
             >
-              <Grid item>
-                <Link
-                  href="/login"
-                  variant="body2"
-                >
-                  既にアカウントをお持ちの方
-                </Link>
-              </Grid>
+              <TextField
+                required
+                fullWidth
+                id="firstName"
+                label="名"
+                name="firstName"
+                autoComplete="family-name"
+              />
             </Grid>
-          </Box>
+            <Grid
+              item
+              xs={12}
+            >
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="メールアドレス"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+            >
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="パスワード"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value="allowExtraEmails"
+                    color="primary"
+                  />
+                }
+                label="メールマガジンを受け取りますか？"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={signupMutation.isLoading}
+          >
+            {signupMutation.isLoading ? '登録中...' : '登録'}
+          </Button>
+          <Grid
+            container
+            justifyContent="flex-end"
+          >
+            <Grid item>
+              <Link
+                href="/login"
+                variant="body2"
+              >
+                既にアカウントをお持ちの方
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
-    </ThemeProvider>
+      </Box>
+    </Container>
   )
 }
